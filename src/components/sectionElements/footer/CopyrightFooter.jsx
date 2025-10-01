ea714f76f86d56399be5e007029cc81e61e9d5c8
Gabriel de Souza <154693365+BiellSouza@users.noreply.github.com>
2025-10-01 20:48:07 -0300

    Initial commit

---
+++ src/components/sectionElements/footer/CopyrightFooter.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { infos } from "../../../content/content"; // importa direto a const infos
+
+function CopyrightFooter() {
+  const { t } = useTranslation();
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <div className="text-center w-full phone2:w-[90%] max-w-[1110px] flex flex-col mb-[48px] phone2:mb-[80px] tablet1:mb-[27px]">
+      {/* <p className="w-full opacity-75">
+        {t("footer.copyrightLine", { year: currentYear, name: infos.name })}
+      </p>
+      <p>
+        <a
+          className="transition hover:underline"
+          target="_blank"
+          href="https://www.paperstreet.com.br"
+        >
+          {t("footer.footerDivulgacaoText")}
+        </a>
+      </p> */}
+
+      <p className="w-full opacity-75">
+        Excelência ampliada: a Gomíde Advocacia & Compliance Jurídico conta com
+        parcerias estratégicas nacionais e auditoria com certificação
+        internacional, assegurando padrões elevados de atuação.
+        <br />
+        <br />© 2025 Gomíde – Advocacia & Compliance Jurídico
+      </p>
+    </div>
+  );
+}
+
+export default CopyrightFooter;
