0eb239bfc46b8ed8dd0eb375e6eeb2dfe5290af3
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-12 17:43:08 -0300

    fix: Cor da Shadow alterada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,113 +1,113 @@
 /** @type {import('tailwindcss').Config} */
-export const content = ["./src/**/*.{js,jsx,ts,tsx}"];
+export const content = ['./src/**/*.{js,jsx,ts,tsx}']
 export const theme = {
   extend: {
     colors: {
       // Principais
 
-      primary: "#4B0F1F",
-      buttonColor: "#D4AF37",
-      minititleDark: "#0A2A43", //bg minitag de seções escuras
-      minititle: "#18252E", //bg minitag de seções claras
-      secondary: "#000000",
-      colorNavbar: "#4B0F1F",
-      colorFooter: "#D4AF37",
-      colorTexto: "#0A2A43",
-      colroTextoSecundario: "#FFFFFF",
-      textButtonsFeature: "#D4AF37",
-      labelFeature: "#CBA46A",
-      bgLogo: "#18252E",
+      primary: '#4B0F1F',
+      buttonColor: '#D4AF37',
+      minititleDark: '#0A2A43', //bg minitag de seções escuras
+      minititle: '#18252E', //bg minitag de seções claras
+      secondary: '#000000',
+      colorNavbar: '#4B0F1F',
+      colorFooter: '#D4AF37',
+      colorTexto: '#0A2A43',
+      colroTextoSecundario: '#FFFFFF',
+      textButtonsFeature: '#D4AF37',
+      labelFeature: '#CBA46A',
+      bgLogo: '#18252E',
 
       // Fundos de Seção
 
-      bgSectionDark: "#FFFFFF",
-      bgSectionOpacityLight: "#F0F0F0",
-      bgFixedLight: "#E8E8E8",
-      bgSectionOpacityDark: "#363636",
-      bgFixedDark: "#101B23",
-      bgSectionLight: "#18252E",
+      bgSectionDark: '#FFFFFF',
+      bgSectionOpacityLight: '#F0F0F0',
+      bgFixedLight: '#E8E8E8',
+      bgSectionOpacityDark: '#363636',
+      bgFixedDark: '#101B23',
+      bgSectionLight: '#18252E',
 
       // Degradê da Lpv
 
-      lpvHeroGradientTop: "#000",
-      lpvHeroGradientBot: "#000",
+      lpvHeroGradientTop: '#000',
+      lpvHeroGradientBot: '#000',
 
       // Botões e Links
 
-      links: "#0189BB",
-      iconButtons: "#D4AF37",
-      labelButtons: "#000",
-      minititleLightLabel: "#D4AF37", //label das minitag de seções claras
-      minititleDarkLabel: "#D4AF37", //label minitag de seções escuras
+      links: '#0189BB',
+      iconButtons: '#D4AF37',
+      labelButtons: '#000',
+      minititleLightLabel: '#D4AF37', //label das minitag de seções claras
+      minititleDarkLabel: '#D4AF37', //label minitag de seções escuras
 
       // Sombras
 
-      shadowHero: "#D4AF37",
-      shadowFeatures: "#000000",
-      shadowAbouts: "#000000",
-      shadowSteps: "#000000",
-      shadowMaps: "#000000",
-      shadowBlogs: "#000000",
+      shadowHero: '#000005',
+      shadowFeatures: '#000000',
+      shadowAbouts: '#000000',
+      shadowSteps: '#000000',
+      shadowMaps: '#000000',
+      shadowBlogs: '#000000',
 
       // Outros
 
-      lighter: "#FFFFFF",
-      darker: "#000000",
+      lighter: '#FFFFFF',
+      darker: '#000000',
     },
     boxShadow: {
-      "custom-opacity": "-20px 20px 0px", // 30% de opacidade
-      "custom-opacityButton": "-5px 5px 0px", // 30% de opacidade no Botão
+      'custom-opacity': '-20px 20px 0px', // 30% de opacidade
+      'custom-opacityButton': '-5px 5px 0px', // 30% de opacidade no Botão
     },
     fontFamily: {
-      mainFont: ["Merriweather", "sans-serif"],
+      mainFont: ['Merriweather', 'sans-serif'],
       // mainFont: ["Poppins", "sans-serif"],
-      secondFont: ["Roboto", "sans-serif"],
-      league: ["League Gothic"],
-      barlow: ["Barlow Condensed"],
+      secondFont: ['Roboto', 'sans-serif'],
+      league: ['League Gothic'],
+      barlow: ['Barlow Condensed'],
     },
     fontSize: {
-      paragraph1: "0.625rem",
-      paragraph2: "0.75rem",
-      paragraph3: "0.875rem",
-      paragraph4: "1rem",
-      paragraph5: "1.125rem",
-      title1: "1.25rem",
-      title2: "1.375rem",
-      title3: "1.5rem",
-      title4: "1.875rem",
-      title5: "2.25rem",
-      title6: "3rem",
-      title7: "3.5rem", // 55px
+      paragraph1: '0.625rem',
+      paragraph2: '0.75rem',
+      paragraph3: '0.875rem',
+      paragraph4: '1rem',
+      paragraph5: '1.125rem',
+      title1: '1.25rem',
+      title2: '1.375rem',
+      title3: '1.5rem',
+      title4: '1.875rem',
+      title5: '2.25rem',
+      title6: '3rem',
+      title7: '3.5rem', // 55px
     },
     spacing: {
-      112: "28rem",
-      120: "30rem",
-      128: "32rem",
+      112: '28rem',
+      120: '30rem',
+      128: '32rem',
     },
     screens: {
-      phone1: "320px",
-      phone2: "375px",
-      phone3: "425px",
-      tablet1: "640px",
-      tablet2: "768px",
-      desktop1: "1024px",
-      desktop2: "1280px",
-      desktop3: "1440px",
+      phone1: '320px',
+      phone2: '375px',
+      phone3: '425px',
+      tablet1: '640px',
+      tablet2: '768px',
+      desktop1: '1024px',
+      desktop2: '1280px',
+      desktop3: '1440px',
     },
     keyframes: {
       slideDown: {
-        "0%": { transform: "translateY(-5%)", opacity: "0" },
-        "100%": { transform: "translateY(0%)", opacity: "1" },
+        '0%': { transform: 'translateY(-5%)', opacity: '0' },
+        '100%': { transform: 'translateY(0%)', opacity: '1' },
       },
       slideUp: {
-        "0%": { transform: "translateY(0%)", opacity: "1" },
-        "100%": { transform: "translateY(-5%)", opacity: "0" },
+        '0%': { transform: 'translateY(0%)', opacity: '1' },
+        '100%': { transform: 'translateY(-5%)', opacity: '0' },
       },
     },
     animation: {
-      "slide-down": "slideDown 1s ease",
-      "slide-up": "slideUp 1s ease",
+      'slide-down': 'slideDown 1s ease',
+      'slide-up': 'slideUp 1s ease',
     },
   },
-};
+}
 // export const plugins = [require("tailwindcss-gradients")];
